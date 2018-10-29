f819c134e2bd11fa0c24613c6ea0d7ea3f2b1d05
Philipp Siegmund <philipp@zauberware.com>
2018-10-29 14:53:15 +0100

    quickFIX

--- src/Icon.js
+++ src/Icon.js
@@ -6,17 +6,11 @@ class Icon extends React.Component {
     const { width, color, icon } = this.props
     const { icons } = this.context
     console.log(icons)
-    console.log(this.props)
     return (
-      <div>  
-        svgOut
-        <IconContext.Consumer>
-          {({ icons }) => (
-            <svg width={width} height={width} viewBox="0 0 1024 1024">
-              <path d={icons[icon]}></path>
-            </svg>
-          )}
-        </IconContext.Consumer>
+      <div>
+        <svg width={width} height={width} viewBox={`0 0 ${width} ${width}`} fill={color}>
+          <path d={icons[icon]}></path>
+        </svg>
       </div>
     )
   }
--- src/IconProvider.js
+++ src/IconProvider.js
@@ -3,12 +3,21 @@ import PropTypes from 'prop-types'
 import IconContext from './IconContext'
 
 class IconProvider extends React.Component {
+  
+  // you must specify what you’re adding to the context
+  static childContextTypes = {
+    icons: PropTypes.object.isRequired,
+  }
+
+  getChildContext() {
+    const { icons } = this.props
+    return { icons }
+  }
+
   render() {
-    return (
-      <IconContext.Provider value={this.props.icons}>
-        {this.props.children}
-      </IconContext.Provider>
-    )
+    // `Children.only` enables us not to add a <div /> for nothing
+    const { children } = this.props
+    return Children.only(children)
   }
 }
 
